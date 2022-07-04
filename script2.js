fcbec87b77a6c41d431aef493618a92c24a3135a
Gokulcasper <ronitcasper215@gmail.com>
2022-07-04 18:17:17 +0530

    Feat : FormData to Server

--- script2.js
+++ script2.js
@@ -6,15 +6,11 @@ const handleSubmit = (event) => {
 
   const formData = new FormData(formEl);
   // 1. QueryString : content-type : application/x-www-form-urlencoded
-  //   ?fullname=Felix+Noel&type=General+FeedBack&email=jihecuzeta%40mailinator.com&description=Ut+eu+sed+fuga+Inci&terms=on
+  // ?fullname=Felix+Noel&type=General+FeedBack&email=jihecuzeta%40mailinator.com&description=Ut+eu+sed+fuga+Inci&terms=on
   const data = [...formData.entries()];
 
-  //   const dataString = data
-  //     .map(
-  //       ([key, value]) =>
-  //         `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-  //     )
-  //     .join("&"); // Old way
+  //   const dataString = data.map(([key, value]) =>`${encodeURIComponent(key)}=${encodeURIComponent(value)}`).join("&"); // Old way
+
   //   console.log("Using Map : ", dataString);
 
   //   const dataString2 = new URLSearchParams(formData).toString();
